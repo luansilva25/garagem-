8eec76a4fb79f8a8bcc9aef7fa30dbc73de6383b
luansilva25 <luansilva250807@gmail.com>
2024-04-05 18:08:56 -0300

    correcao

--- backend/garagem/models/veiculo.py
+++ backend/garagem/models/veiculo.py
@@ -4,7 +4,7 @@ from garagem import models as itens
 class Veiculo(models.Model):
     marca = models.ForeignKey(itens.Marca, on_delete=models.PROTECT, related_name="marca")
     categoria = models.ForeignKey(itens.Categoria, on_delete=models.PROTECT, related_name="categoria")
-    cor = models.ForeignKey(itens.Categoria, on_delete=models.PROTECT,related_name="cor")
+    cor = models.ForeignKey(itens.Cor, on_delete=models.PROTECT,related_name="cor")
     acessorio = models.ManyToManyField(itens.Acessorio)
     ano = models.IntegerField(default=0,null=True, blank=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
